722bb727d2a25ae6765041156c2355aaa442527a
RyanGualberto <RyanOliveiraGualberto@gmail.com>
2022-04-08 23:54:17 -0300

    Firstt

--- components/Componente.js
+++ components/Componente.js
@@ -1,24 +1,30 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native-web';
 
-export default function Calcular() {
-    function calc(){
-    const [Convidados, setConvidados] = useState();
-    var qtrefri = Convidados * 600;
-    var qtagua = Convidados * 200;
-    var qtbolo = Convidados * 100;
-    var qtdoces = Convidados * 3;
-    var qtsalg = Convidados * 10;
-    
+export default function Calcular(props) {
+    let value = [];
+    function calc() {
 
 
-}
+          
+            var qtrefri = Convidados * 600;
+        var qtagua = Convidados * 200;
+        var qtbolo = Convidados * 100;
+        var qtdoces = Convidados * 3;
+        var qtsalg = Convidados * 10;
+      
+        <Text> Planejamento Festa</Text>;
+        <Text> Refrigerante: {qtrefri}ml</Text>;
+        <Text> Agua: {qtagua}ml</Text>;
+        <Text> Bolo: {qtbolo} gramas</Text>;
+        <Text> Doces: {qtdoces} Unidades</Text>;
+        <Text> Salgadinhos: {qtsalg} unidades</Text>;
+
+
+    }
 
     return (
-        
-      <View style={styles.container}>
-        <Text> planejamento Festa</Text>
-        <Text> 
-      </View>
+
+       <View>  {calc} </View>
     );
-  }
+}
