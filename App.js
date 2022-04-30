b174a3f5386b1d5f8e05c2a67f8a6a086fb3794e
Ryan Gualberto <ryanOliveiraGualberto@gmail.com>
2022-04-29 23:57:18 -0300

    1.1 falta ativar o botao

--- App.js
+++ App.js
@@ -9,29 +9,63 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <View> Digite a quantidade de Convidados</View>
-      <TextInput placeholder="quantidade" style={styles.entradaqtd} value={Convidados} onChangeText={(value) => setConvidados(value)}></TextInput>
+    <View style={styles.h1}> Party Calc</View>
+      <View style={styles.h2}> Digite a quantidade de Convidados</View>
+      <TextInput placeholder="quantidade" style={styles.input} value={Convidados} onChangeText={(value) => setConvidados(value)}></TextInput>
       
 
       <TouchableOpacity style={styles.botao} onPress={mostrarTexto()}>
-          <View>Calcular</View>
+          <View style={styles.txtbotao}>Calcular</View>
         </TouchableOpacity>
         <Componente qtConvidados={Convidados} />
     </View>
   );
 }
-
+{/* cores : amarelo : #ffda7e, branco: #ffff00, verde: #d9eec9, rosa: #fdd6d0, azul: #83d6fc*/}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1c1c',
+    backgroundColor: '#fffff0',
     alignItems: 'center',
-    justifyContent: 'center',
+    fontFamily: 'arial'
+
   },
-  entradaqtd: {
-    backgroundColor: 'red',
+  h1:{
+    fontSize: '48px',
+    color: '#83d6fc',
+    fontweight: 'bold',
+    top: '50px',
+    fontFamily: 'Courier New',
+  },
+  h2: {
+    color: '#09EE0C',
+    fontSize: '20px',
+    fontWeight: 'bold',
+    marginTop: '120px'
+  },
+  input: {
+    textAlign: 'center',
+    backgroundColor: '#ffda7e',
+    borderRadius:'5px',
+    border: '3px solid #fdd6d0',
+    marginTop: '20px',
+    width: '280px',
+    height: '35px',
   },
   botao: {
-    backgroundColor: 'red',
+    backgroundColor: '#83d6fc',
+    width: '100px',
+    height: '35px',
+    textAlign: 'center',
+    justifyContent: 'center',
+    border: '2px solid  #fdd6d0',
+    borderRadius: '5px',
+    fontSize: '22px',
+    marginTop: '20px'
+  },
+
+  txtbotao: {
+    Color: '#ffda7e',
+    fontweight: 'bold',
   }
 });
--- components/Componente.js
+++ components/Componente.js
@@ -1,30 +1,32 @@
 import React, { useState } from 'react';
-import { Text, View } from 'react-native-web';
+import { StyleSheet,Text, View } from 'react-native-web';
 
 export default function Componente(props) {
     let texto = [];
 
 
-    let qtrefri = props.qtConvidados * 600;
-    let qtagua = props.qtConvidados * 200;
-    let qtbolo = props.qtConvidados * 100;
+    let qtrefri = (props.qtConvidados * 600) / 1000;
+    let qtagua = (props.qtConvidados * 200) / 1000;
+    let qtbolo = (props.qtConvidados * 100) / 1000; 
     let qtdoces = props.qtConvidados * 3;
     let qtsalg = props.qtConvidados * 10;
     texto.push(
-        <Text> 
-            quantidade de pessoas {props.qtConvidados}
+        <View style={styles.container}>
+        <Text style={styles.Answer}> 
+            quantidade de pessoas <View style={styles.valores}>{props.qtConvidados}</View>
             <br></br>
-            Refrigerante: {qtrefri}ml
+            Refrigerante: <View style={styles.valores}>{qtrefri}</View>L
             <br></br>
-            Agua: {qtagua}ml
+            Agua: <View style={styles.valores}>{qtagua}</View>L
             <br></br>
-            Bolo: {qtbolo} gramas
+            Bolo: <View style={styles.valores}>{qtbolo}</View> Kg
             <br></br>
-            Doces: {qtdoces} Unidades;
+            Doces: <View style={styles.valores}>{qtdoces}</View> Unidades;
             <br></br>
-            Salgadinhos: {qtsalg} unidades
+            Salgadinhos: <View style={styles.valores}>{qtsalg}</View> unidades
             <br></br>
             </Text>
+            </View>
     );
 
     return (
@@ -32,3 +34,22 @@ export default function Componente(props) {
         <View>  {texto} </View>
     );
 }
+
+const styles = StyleSheet.create({ 
+    Answer: {
+        
+        marginTop: '15px',
+        width: '300px',
+        height: '250px',
+        backgroundColor: '#d9eec9',   
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '22px',
+        textAlign: 'justify',
+        fontWeight: 'bold', 
+ 
+    },
+    valores: {
+        color: '#E5DA05'
+    }
+});
